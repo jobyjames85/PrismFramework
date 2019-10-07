dcc0349006612d0d04bc7f745967656d36091cfb
joby.james <joby.james@fingent.com>
2019-10-07 15:46:10 +0530

    Add project files.

---
+++ CaseInstaller/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using CaseInstaller.View;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace CaseInstaller
+{
+    public partial class App : PrismApplication
+    {
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        ////protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        ////{
+        ////    moduleCatalog.AddModule<ModuleAModule>();
+        ////}
+    }
+}
